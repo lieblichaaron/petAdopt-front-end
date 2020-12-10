885f5c31651bd6dd2f4ab615c02af64e399f5e0a
Aaron Lieblich <lieblichaaron@gmail.com>
2020-12-10 12:14:15 +0200

    create Homepage Logged out

---
+++ src/Components/HomepageLoggedOut.jsx
@@ -0,0 +1,10 @@
+const HomepageLoggedOut = () => {
+    return ( 
+        <div>
+        <Button>SignUp</Button>
+        <Button>Login</Button>
+        </div>
+     );
+}
+ 
+export default HomepageLoggedOut;
