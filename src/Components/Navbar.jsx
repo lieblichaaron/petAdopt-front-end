000e0a9be1d2d2d4dd5d9bbc53c3b94c09fb67d3
Aaron Lieblich <lieblichaaron@gmail.com>
2020-12-14 22:44:00 +0200

    replace query params with location

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,10 +3,7 @@ import { NavLink, useLocation } from "react-router-dom";
 import Logo from "../images/favicon-32x32.png";
 import styles from "./Navbar.module.css";
 const CustomNavbar = () => {
-  function useQuery() {
-    return new URLSearchParams(useLocation().search);
-  }
-  let query = useQuery();
+  const location = useLocation();
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand>
@@ -14,25 +11,22 @@ const CustomNavbar = () => {
         PetAdopt
       </Navbar.Brand>
       <Nav className="mr-auto">
-        {query.get("name") !== "Home" && (
-          <NavLink className={styles["header-link"]} to="/?name=Home">
+        {location.pathname !== "/" && (
+          <NavLink className={styles["header-link"]} to="/">
             Home
           </NavLink>
         )}
-        {query.get("name") !== "PetSearch" && (
-          <NavLink
-            className={styles["header-link"]}
-            to="/PetSearch?name=PetSearch"
-          >
+        {location.pathname !== "/PetSearch" && (
+          <NavLink className={styles["header-link"]} to="/PetSearch">
             Find a pet
           </NavLink>
         )}
-        {query.get("name") !== "MyPets" && (
-          <NavLink className={styles["header-link"]} to="/MyPets?name=MyPets">
+        {location.pathname !== "/MyPets" && (
+          <NavLink className={styles["header-link"]} to="/MyPets">
             My pets
           </NavLink>
         )}
-        {query.get("name") !== "ProfileSettings" && (
+        {location.pathname !== "/ProfileSettings" && (
           <NavLink
             className={styles["header-link"]}
             to="/ProfileSettings?name=ProfileSettings"
@@ -40,8 +34,8 @@ const CustomNavbar = () => {
             Profile settings
           </NavLink>
         )}
-        {query.get("name") !== "About" && (
-          <NavLink className={styles["header-link"]} to="/About?name=About">
+        {location.pathname !== "/About" && (
+          <NavLink className={styles["header-link"]} to="/About">
             About us
           </NavLink>
         )}
