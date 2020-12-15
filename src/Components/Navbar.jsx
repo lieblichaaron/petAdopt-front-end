21e872a1ac710c2d084fbb13a8e7aa4dff54fb4d
Aaron Lieblich <lieblichaaron@gmail.com>
2020-12-15 16:28:50 +0200

    remove conditions from navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,6 @@ import { NavLink, useLocation } from "react-router-dom";
 import Logo from "../images/favicon-32x32.png";
 import styles from "./Navbar.module.css";
 const CustomNavbar = () => {
-  const location = useLocation();
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand>
@@ -11,34 +10,28 @@ const CustomNavbar = () => {
         PetAdopt
       </Navbar.Brand>
       <Nav className="mr-auto">
-        {location.pathname !== "/" && (
-          <NavLink className={styles["header-link"]} to="/">
-            Home
-          </NavLink>
-        )}
-        {location.pathname !== "/PetSearch" && (
-          <NavLink className={styles["header-link"]} to="/PetSearch">
-            Find a pet
-          </NavLink>
-        )}
-        {location.pathname !== "/MyPets" && (
-          <NavLink className={styles["header-link"]} to="/MyPets">
-            My pets
-          </NavLink>
-        )}
-        {location.pathname !== "/ProfileSettings" && (
-          <NavLink
-            className={styles["header-link"]}
-            to="/ProfileSettings?name=ProfileSettings"
-          >
-            Profile settings
-          </NavLink>
-        )}
-        {location.pathname !== "/About" && (
-          <NavLink className={styles["header-link"]} to="/About">
-            About us
-          </NavLink>
-        )}
+        <NavLink className={styles["header-link"]} to="/">
+          Home
+        </NavLink>
+
+        <NavLink className={styles["header-link"]} to="/PetSearch">
+          Find a pet
+        </NavLink>
+
+        <NavLink className={styles["header-link"]} to="/MyPets">
+          My pets
+        </NavLink>
+
+        <NavLink
+          className={styles["header-link"]}
+          to="/ProfileSettings?name=ProfileSettings"
+        >
+          Profile settings
+        </NavLink>
+
+        <NavLink className={styles["header-link"]} to="/About">
+          About us
+        </NavLink>
       </Nav>
     </Navbar>
   );
