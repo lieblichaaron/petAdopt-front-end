7a68aebb6048c282f8b198bc89a45d67957abca7
Aaron Lieblich <lieblichaaron@gmail.com>
2020-12-21 20:10:45 +0200

    finish add pet form

--- src/Components/AddPet.jsx
+++ src/Components/AddPet.jsx
@@ -1,22 +1,54 @@
 import { useState } from "react";
 import CustomNavbar from "./Navbar";
-import { Form, Button, Card, Dropdown } from "react-bootstrap";
+import { Form, Button, Card, DropdownButton, Dropdown } from "react-bootstrap";
 import styles from "./AddPet.module.css";
 
 const AddPet = () => {
-  const [type, setType] = useState("");
-  const [name, setName] = useState("");
-  const [adoptionStatus, setAdoptionStatus] = useState("");
-  const [height, setHeight] = useState("");
-  const [weight, setWeight] = useState("");
-  const [color, setColor] = useState("");
-  const [hypoallergenic, setHypoallergenic] = useState("");
-  const [dietaryRestrictions, setDietaryRestrictions] = useState("");
-  const [breed, setBreed] = useState("");
-  const [picture, setPicture] = useState("");
-  const [bio, setBio] = useState("");
+  const formFields = {
+    type: "",
+    name: "",
+    adoptionStatus: "",
+    picture: "",
+    height: "",
+    weight: "",
+    color: "",
+    bio: "",
+    hypoallergenic: null,
+    dietaryRestrictions: null,
+    breedOfAnimal: "",
+  };
+  const [formInfo, setFormInfo] = useState(formFields);
+  const [adoptionStatus, setAdoptionStatus] = useState("Select");
+  const handleInput = (e) => {
+    if (!e.target) {
+      setAdoptionStatus(e);
+      setFormInfo({
+        ...formInfo,
+        adoptionStatus: e,
+      });
+    } else if (e.target.type === "checkbox") {
+      setFormInfo({
+        ...formInfo,
+        [e.target.name]: e.target.checked,
+      });
+    } else if (e.target.type === "file") {
+      setFormInfo({
+        ...formInfo,
+        [e.target.name]: e.target.files[0],
+      });
+    } else {
+      let value = e.target.value[0]
+        ? e.target.value[0].toUpperCase() + e.target.value.substring(1)
+        : e.target.value;
+      setFormInfo({
+        ...formInfo,
+        [e.target.name]: value,
+      });
+    }
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(formInfo);
   };
   return (
     <div>
@@ -27,57 +59,81 @@ const AddPet = () => {
           <Form onSubmit={(e) => handleSubmit(e)}>
             <Form.Group id="type">
               <Form.Label>Type</Form.Label>
-              <Form.Control />
+              <Form.Control
+                name="type"
+                type="text"
+                onChange={(e) => handleInput(e)}
+              />
             </Form.Group>
             <Form.Group id="name">
               <Form.Label>Name</Form.Label>
-              <Form.Control />
+              <Form.Control name="name" type="text" onChange={handleInput} />
             </Form.Group>
             <Form.Group id="adoption status">
               <Form.Label>Adoption Status</Form.Label>
-              <Dropdown>
-                <Dropdown.Toggle variant="info" id="dropdown-basic">
-                  Choose one
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                  <Dropdown.Item>Adopted</Dropdown.Item>
-                  <Dropdown.Item>Fostered</Dropdown.Item>
-                  <Dropdown.Item>Looking for a new home</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+              <DropdownButton
+                variant="info"
+                id="dropdown-basic-button"
+                onSelect={handleInput}
+                title={adoptionStatus}
+              >
+                <Dropdown.Item eventKey="Adopted">Adopted</Dropdown.Item>
+                <Dropdown.Item eventKey="Fostered">Fostered</Dropdown.Item>
+                <Dropdown.Item eventKey="Looking for a new home">
+                  Looking for a new home
+                </Dropdown.Item>
+              </DropdownButton>
             </Form.Group>
             <Form.Group id="height">
-              <Form.Label>Height</Form.Label>
-              <Form.Control />
+              <Form.Label>Height(cm)</Form.Label>
+              <Form.Control name="height" type="text" onChange={handleInput} />
             </Form.Group>
             <Form.Group id="weight">
-              <Form.Label>Weight</Form.Label>
-              <Form.Control />
+              <Form.Label>Weight(kg)</Form.Label>
+              <Form.Control name="weight" type="text" onChange={handleInput} />
             </Form.Group>
             <Form.Group id="color">
               <Form.Label>Color</Form.Label>
-              <Form.Control />
+              <Form.Control name="color" type="text" onChange={handleInput} />
             </Form.Group>
             <Form.Group id="hypoallergenic">
-              <Form.Label>Hypoallergenic</Form.Label>
-              <Form.Check />
+              <Form.Label>Hypoallergenic (check if true)</Form.Label>
+              <Form.Check
+                name="hypoallergenic"
+                type="checkbox"
+                onChange={handleInput}
+              />
             </Form.Group>
             <Form.Group id="dietary restrictions">
-              <Form.Label>Dietary restrictions</Form.Label>
-              <Form.Control />
+              <Form.Label>
+                Dietary restrictions (leave empty if none)
+              </Form.Label>
+              <Form.Control
+                name="dietaryRestrictions"
+                type="text"
+                onChange={handleInput}
+              />
             </Form.Group>
             <Form.Group id="breed of animal">
               <Form.Label>Breed of animal</Form.Label>
-              <Form.Control />
+              <Form.Control
+                name="breedOfAnimal"
+                type="text"
+                onChange={handleInput}
+              />
             </Form.Group>
             <Form.Group id="picture">
               <Form.Label>Picture</Form.Label>
-              <Form.Control type="file" />
+              <Form.Control name="picture" type="file" onChange={handleInput} />
             </Form.Group>
             <Form.Group id="bio">
               <Form.Label>Bio</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control
+                name="bio"
+                as="textarea"
+                rows={3}
+                onChange={handleInput}
+              />
             </Form.Group>
             <Button type="Submit" className="w-100">
               Save changes
